03547608c3f2ede5d8c0f25af80c0a9f53f3a760
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-08-15 22:59:05 +0530

    Add loops for pydle function with new colors
    Added loops to call pydle function with new color parameters.

--- a.py
+++ a.py
@@ -8,3 +8,13 @@ for x in [1, 3]:
     for y in [1, 3]:
         pydle(x, y, "", "yellow")
 for x,y in [(1,2),(2,1),(2,2),(2,3),(3,2)]: pydle(x,y,"","black")
+for x in range(5):
+    for y in range(5):
+        pydle(x, y, "", "orange")
+for x in [0, 4]:
+    for y in [0, 4]:
+        pydle(x, y, "", "blue")
+for x in [1, 3]:
+    for y in [1, 3]:
+        pydle(x, y, "", "yellow")
+for x,y in [(1,2),(2,1),(2,2),(2,3),(3,2)]: pydle(x,y,"","black")

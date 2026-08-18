c71c3d3c0f038ee02026ccb7d1405520a6b093fc
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-08-18 20:43:32 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,20 +1,14 @@
-for x in range(5):
-    for y in range(5):
-        pydle(x, y, "", "orange")
-for x in [0, 4]:
-    for y in [0, 4]:
-        pydle(x, y, "", "blue")
-for x in [1, 3]:
-    for y in [1, 3]:
-        pydle(x, y, "", "yellow")
-for x,y in [(1,2),(2,1),(2,2),(2,3),(3,2)]: pydle(x,y,"","black")
-for x in range(5):
-    for y in range(5):
-        pydle(x, y, "", "orange")
-for x in [0, 4]:
-    for y in [0, 4]:
-        pydle(x, y, "", "blue")
-for x in [1, 3]:
-    for y in [1, 3]:
-        pydle(x, y, "", "yellow")
-for x,y in [(1,2),(2,1),(2,2),(2,3),(3,2)]: pydle(x,y,"","black")
+for y in range(5):
+    for x in range(5):
+        if x >= 4:
+            color = "brown"
+        elif y == 0 and x >= 3:
+            color = "brown"
+        elif (y == 1 and x == 0) or (y == 4 and x == 0):
+            color = "white"
+        elif (x >= 2 and y >= 1) or (x >= 1 and y >= 3):
+            color = "red"
+        else:
+            color = "purple"
+
+        pydle(x, y, "", color)

f7fcad76197dba40bea4926d6eb807cbc3012a62
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-08-30 22:34:36 +0530

    Add colors and grid definitions to day 10.py

--- day 10.py
+++ day 10.py
@@ -10,3 +10,16 @@ grid = [
 for y in range(5):
     for x in range(5):
         pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+        

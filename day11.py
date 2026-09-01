12e0daf4b55c738d6959edbaf98c7c205f488542
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-01 21:34:12 +0530

    Update day11.py

--- day11.py
+++ day11.py
@@ -3,3 +3,8 @@ d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
 
 for i in range(25):
     pydle(i % 5, i // 5, "", d[s[i]])
+s = "BBYBBBBYKOYYYBBYYYKORBRBB"
+d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
+
+for i in range(25):
+    pydle(i % 5, i // 5, "", d[s[i]])

a49744ee67f2345c9d9853fbd873c0936e95afd8
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-02 21:24:16 +0530

    Update day11.py

--- day11.py
+++ day11.py
@@ -18,3 +18,23 @@ d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
 
 for i in range(25):
     pydle(i % 5, i // 5, "", d[s[i]])
+s = "BBYBBBBYKOYYYBBYYYKORBRBB"
+d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
+
+for i in range(25):
+    pydle(i % 5, i // 5, "", d[s[i]])
+s = "BBYBBBBYKOYYYBBYYYKORBRBB"
+d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
+
+for i in range(25):
+    pydle(i % 5, i // 5, "", d[s[i]])
+s = "BBYBBBBYKOYYYBBYYYKORBRBB"
+d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
+
+for i in range(25):
+    pydle(i % 5, i // 5, "", d[s[i]])
+s = "BBYBBBBYKOYYYBBYYYKORBRBB"
+d = {"B": "blue", "Y": "yellow", "K": "black", "O": "orange", "R": "brown"}
+
+for i in range(25):
+    pydle(i % 5, i // 5, "", d[s[i]])

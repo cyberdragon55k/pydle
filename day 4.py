9525b05a3e152f3f9f85486e3a7817cb9cba159c
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-03 21:12:15 +0530

    Update day 4.py

--- day 4.py
+++ day 4.py
@@ -7,3 +7,21 @@ for y in range(5):
         char = sy[y][x]
         text = "🔴" if char == "R" else ("" if char == " " else "●")
         pydle(x, y, text, c[bg[y][x]])
+c = {"w": "white", "k": "black", "b": "blue"}
+bg = ["wwwww", "wwwkw", "wwwkw", "wkkbb", "wwwbw"]
+sy = ["RRR  ", "R    ", "R    ", "    ●", "   ● "]
+
+for y in range(5):
+    for x in range(5):
+        char = sy[y][x]
+        text = "🔴" if char == "R" else ("" if char == " " else "●")
+        pydle(x, y, text, c[bg[y][x]])
+c = {"w": "white", "k": "black", "b": "blue"}
+bg = ["wwwww", "wwwkw", "wwwkw", "wkkbb", "wwwbw"]
+sy = ["RRR  ", "R    ", "R    ", "    ●", "   ● "]
+
+for y in range(5):
+    for x in range(5):
+        char = sy[y][x]
+        text = "🔴" if char == "R" else ("" if char == " " else "●")
+        pydle(x, y, text, c[bg[y][x]])

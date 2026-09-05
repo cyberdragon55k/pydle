a673b80152abe787a58cb493618854750b1b9313
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-05 20:29:48 +0530

    Update day 10.py

--- day 10.py
+++ day 10.py
@@ -11,6 +11,32 @@ for y in range(5):
     for x in range(5):
         pydle(x, y, grid[y][x], colors[y])
 colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
 grid = [
     "ABCDE",
     "FGHIJ",

de418ae3afac4932724f09c66eba913101e82d36
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-06 21:54:53 +0530

    Update day 10.py

--- day 10.py
+++ day 10.py
@@ -72,3 +72,225 @@ for y in range(5):
     for x in range(5):
         pydle(x, y, grid[y][x], colors[y])
         
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+        
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+        
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+colors = ["red", "red", "white", "blue", "blue"]
+grid = [
+    "ABCDE",
+    "FGHIJ",
+    "KLMLK",
+    "JIHGF",
+    "EDCBA"
+]
+
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, grid[y][x], colors[y])
+        

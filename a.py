c8091f3001d7a67f70eac52c11c3bd3dabbde9a3
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-07 19:32:18 +0530

    Refactor color assignment logic in a.py

--- a.py
+++ a.py
@@ -12,3 +12,31 @@ for y in range(5):
             color = "purple"
 
         pydle(x, y, "", color)
+for y in range(5):
+    for x in range(5):
+        if x >= 4:
+            color = "brown"
+        elif y == 0 and x >= 3:
+            color = "brown"
+        elif (y == 1 and x == 0) or (y == 4 and x == 0):
+            color = "white"
+        elif (x >= 2 and y >= 1) or (x >= 1 and y >= 3):
+            color = "red"
+        else:
+            color = "purple"
+
+        pydle(x, y, "", color)
+for y in range(5):
+    for x in range(5):
+        if x >= 4:
+            color = "brown"
+        elif y == 0 and x >= 3:
+            color = "brown"
+        elif (y == 1 and x == 0) or (y == 4 and x == 0):
+            color = "white"
+        elif (x >= 2 and y >= 1) or (x >= 1 and y >= 3):
+            color = "red"
+        else:
+            color = "purple"
+
+        pydle(x, y, "", color)

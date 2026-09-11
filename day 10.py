5d104c4d37275b01791173ff8bc7759c40ccf99d
Aditya Namdeo <74948198+cyberdragon55k@users.noreply.github.com>
2026-09-11 22:27:00 +0530

    Update day 10.py

--- day 10.py
+++ day 10.py
@@ -8,3 +8,13 @@ colors = {'w': 'white', 'b': 'brown', 'g': 'green', 'r': 'red'}
 for y in range(5):
     for x in range(5):
         pydle(x, y, "", colors[grid[y][x]])
+grid = [
+    "wbwgw",
+    "rrbrr",
+    "rrrrr",
+    "rrrrr",
+    "wrrrw"]
+colors = {'w': 'white', 'b': 'brown', 'g': 'green', 'r': 'red'}
+for y in range(5):
+    for x in range(5):
+        pydle(x, y, "", colors[grid[y][x]])
